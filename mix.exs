d1ef3554d9847d5173c241f2cc4bb7336d33be01
Samuel Richardson <sam@richardson.co.nz>
2026-06-15 14:35:36 +1000

    Update deps
    Upgrades req to 0.6.0 and bumps the others (including ones with vulns)

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule ReqChronicle.MixProject do
       {:bypass, "~> 2.1", only: :test},
       {:ecto_sql, "~> 3.10"},
       {:nimble_options, "~> 1.1"},
-      {:req, "~> 0.5.0"},
+      {:req, "~> 0.6.0"},
       {:styler, "~> 1.0.0-rc.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
